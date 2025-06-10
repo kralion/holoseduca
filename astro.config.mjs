c7669cd4c114521b3be912fbc9f5ff921e8e5953
kralion <joan300501@gmail.com>
2025-06-10 12:17:39 -0500

    Update site configuration and styling with Tailwind CSS integration; change site title and description to reflect HolosEduca's mission. Revamp header and footer components for improved layout and accessibility. Enhance the about and index pages with new content and structure, emphasizing educational resources and community engagement.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://holoseduca.netlify.app',
 	integrations: [mdx(), sitemap()],
+	vite: {
+    plugins: [tailwindcss()],
+  },
 });
