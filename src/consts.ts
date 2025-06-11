0f8bfacc31900ce53b0e683258cebe649c974851
kralion <joan300501@gmail.com>
2025-06-11 12:47:09 -0500

    meta tags

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "HolosEduca";
-export const SITE_DESCRIPTION = "Transforming education in Colombia through innovative learning solutions and accessible resources.";
+export const SITE_DESCRIPTION = "Transformando la educación en Colombia a través de soluciones innovadoras y recursos accesibles.";
